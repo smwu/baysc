ca9f9c1b7065a5fc5e12b0b3995cd6913c93ed37
Stephanie M Wu <swu@g.harvard.edu>
2023-12-08 13:29:27 +0100

    Added nhanes dataset

---
+++ R/data.R
@@ -0,0 +1,178 @@
+#' NHANES diet and hypertension for low-income women 
+#' 
+#' Cleaned dataset containing NHANES 2015-2018 data on dietary intake and 
+#' hypertension among adult women aged 20 or over who are classified as 
+#' low-income (reported household income at or below 185\% of the federal 
+#' poverty level). 
+#' 
+#' @docType data
+#' @usage data(data_nhanes)
+#' @format A dataframe with 2004 rows and 71 variables:
+#' \describe{
+#'   \item{\code{SEQN}}{5- or 6-digit unique individual identifier}
+#'   \item{\code{stratum_id}}{Survey design stratum indicator}
+#'   \item{\code{cluster_id}}{Survey design cluster indicator, modified to be 
+#'   unique across strata using the formula: SDMVSTRA*10 + SDMVPSU}
+#'   \item{\code{sample_wt}}{Individual survey sampling weights}
+#'   \item{\code{age_cat}}{Categorical age variable with three categories: 
+#'   1: 20-39, 2: 40-59, 3: >=60, created from the RIAGEYR variable}
+#'   \item{\code{racethnic}}{Race and Hispanic origin ethnicity with 5 categories:
+#'   1: NH White, 2: NH Black, 3: NH Asian, 4: Hispanic, 5: Other/Mixed, created
+#'   from the RIDRETH3 variable}
+#'   \item{\code{educ}}{Education level with 3 categories: 1: at least some 
+#'   college, 2: high school/GED, 3: less than high school, created from the
+#'   DMDEDUC2 variable}
+#'   \item{\code{smoker}}{Binary current smoking status, defined as 1 (yes)
+#'   if having smoked 100 cigarettes in lifetime and currently smokes cigarettes,
+#'   and 0 (no) otherwise, created from SMQ040 and SMQ020 variables}
+#'   \item{\code{physactive}}{Physically active as a factor with levels "Inactive"
+#'   and "Active", with active defined as having at least 150 minutes of moderate
+#'   or vigorous exercise per week}
+#'   \item{\code{BP_flag}}{Binary 0/1 hypertension variable, with 1 (yes) 
+#'   defined as having average systolic blood pressure (BP) above 130, having 
+#'   average diastolic BP above 80, currently taking BP medication, or having 
+#'   been told have high BP at least two times}
+#'   \item{\code{citrus}}{Consumption of citrus, melons, or berries with 4
+#'   categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{oth_fruit}}{Consumption of other fruits with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{fruit_juice}}{Consumption of fruit juices with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{dark_green}}{Consumption of dark green vegetables with 4
+#'   categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{tomatoes}}{Consumption of tomatoes and tomato products with 4
+#'   categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{oth_red}}{Consumption of other red and orange vegetables with 4
+#'   categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{potatoes}}{Consumption of white potatoes with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{oth_starchy}}{Consumption of other starchy vegetables with 4
+#'   categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{oth_veg}}{Consumption of other vegetables with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{whole_grain}}{Consumption of whole grains with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{ref_grain}}{Consumption of refined grains with 4
+#'   categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{meat}}{Consumption of beef, veal, pork, lamb, and game meat, 
+#'   excluding organ and cured meat, with 4 categories: 1) None, 2) Low, 3) Med, 
+#'   4) High}
+#'   \item{\code{cured_meats}}{Consumption of cured meats with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{organ}}{Consumption of organ meat from beef, veal, pork, lamb,
+#'   game, and poultry with 4 categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{poultry}}{Consumption of poultry, excluding organ and cured 
+#'   meat, with 4 categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{seafood_high}}{Consumption of seafood high in n-3 fatty acids, 
+#'   with 4 categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{seafood_low}}{Consumption of seafood low in n-3 fatty acids, 
+#'   with 4 categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{eggs}}{Consumption of eggs and egg substitutdes, with 4 
+#'   categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{soybean}}{Consumption of soy products, excluding soymilk and 
+#'   products made with raw soybean, with 4 categories: 1) None, 2) Low, 3) Med, 
+#'   4) High}
+#'   \item{\code{nuts}}{Consumption of nuts and seeds with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{leg_protein}}{Consumption of legumes (beans, peas, lentils) 
+#'   computed as protein foods, with 4 categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{milk}}{Consumption of milk and soymilk, with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{yogurt}}{Consumption of yogurt, with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{cheese}}{Consumption of cheese, with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{oils}}{Consumption of oils, with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{solid_fats}}{Consumption of solid fats, with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{add_sugars}}{Consumption of added sugars, with 4 categories: 
+#'   1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{drinks}}{Consumption of alcoholic beverages and alcohol added 
+#'   to foods after cooking, with 4 categories: 1: None, 2: Low, 3: Med, 4: High}
+#'   \item{\code{cycle}}{NHANES two-year cycle, either "2015-2016" or "2017-2018"}
+#'   \item{\code{nrecall}}{Number of 24-hr dietary recalls completed by the 
+#'   individual}
+#'   \item{\code{SDMVSTRA}}{Original NHANES stratum indicator}
+#'   \item{\code{SDMVPSU}}{Original NHANES cluster indicator}
+#'   \item{\code{RIAGENDR}}{Original NHANES gender variable. Equal to 2 for all 
+#'   individuals because restricted to women.}
+#'   \item{\code{RIDAGEYR}}{Original NHANES age in years. Restricted to those 
+#'   age 20 or over.}
+#'   \item{\code{RIDRETH3}}{Original NHANES race/ethnicty variable with 7 
+#'   categories: 1=Mex_Amer, 2=Other_Hisp, 3=NH_White, 4=NH_Black, 6=NH_Asian, 
+#'   7=Other/Mixed}
+#'   \item{\code{DMDEDUC2}}{Original NHANES education level with categories:
+#'   1= <9th, 2=9-11th, 3=HS/GED, 4=Some college/AA, 5=college grad or above, 
+#'   7=refused, 9=don't know}
+#'   \item{\code{SMQ020}}{Original NHANES binary variable of having smoked at 
+#'   least 100 cigarettes in life}
+#'   \item{\code{SMQ040}}{Original NHANES binary variable of currently smoking
+#'   cigarettes}
+#'   \item{\code{Mins_Active}}{Number of minutes of moderate or vigorous 
+#'   exercise per week, created from NHANES variables PAQ610, PAQ615,
+#'   PAQ625, PAQ630, PAQ640, PAQ645, PAQ655, PAQ660, PAQ670, PAQ675}
+#'   \item{\code{SBP_avg}}{Average systolic BP over four readings, created from
+#'   NHANES variables BPXSY1, BPXSY2, BPXSY3, BPXSY4}
+#'   \item{\code{DBP_avg}}{Average diastolic BP over four readings, created from
+#'   NHANES variables BPXDI1, BPXDI2, BPXDI3, BPXDI4}
+#'   \item{\code{BPQ030}}{Original NHANES binary variable of having been told 
+#'   have high BP at least two times}
+#'   \item{\code{BPQ050A}}{Original NHANES binary variable of currently taking
+#'   prescribed medication for high BP}
+#'   \item{\code{BPQ020}}{Original NHANES binary variable of having ever been 
+#'   told have high BP}
+#' }
+#' @source https://github.com/smwu/SWOLCA/tree/main/Data. For more information 
+#' on dataset preparation, see Wu et al. (2023)
+#' @references Wu, S. M., Williams, M. R., Savitsky, T. D., & Stephenson, B. J. 
+#' (2023). Derivation of outcome-dependent dietary patterns for low-income women 
+#' obtained from survey data using a Supervised Weighted Overfitted Latent Class 
+#' Analysis. arXiv preprint arXiv:2310.01575.
+#' @keywords datasets
+#' @examples 
+#' data(data_nhanes)
+#' x_mat <- data_nhanes %>% dplyr::select(citrus:drinks)
+#' y_all <- data_nhanes$BP_flag
+"data_nhanes"
+
+
+
+#' Simulated data
+#' 
+#' A dataset simulated using `simulate_data.R`
+#' @docType data
+#' @usage data(sim_data)
+#' @format A list with 20 elements:
+#' \describe{
+#'   \item{\code{samp_ind}}{Vector of population indices for sampled individuals. nx1}
+#'   \item{\code{sample_wt}}{Vector of individual survey sampling weights. nx1}
+#'   \item{\code{true_Si}}{Vector of stratum indicators. nx1}
+#'   \item{\code{cluster_id}}{Vector of cluster indicators. nx1}
+#'   \item{\code{X_data}}{Matrix of multivariate categorical exposure. nxp}
+#'   \item{\code{Y_data}}{Vector of outcomes. nx1}
+#'   \item{\code{N}}{Population size}
+#'   \item{\code{N_s}}{Vector of stratum-specific population sizes. Sx1}
+#'   \item{\code{p}}{Number of exposure items}
+#'   \item{\code{d}}{Number of exposure categories}
+#'   \item{\code{S}}{Number of strata}
+#'   \item{\code{true_K}}{True number of latent classes}
+#'   \item{\code{true_pi_s}}{Matrix of true class membership probabilities by stratum. SxK}
+#'   \item{\code{true_pi}}{Vector of true class membership probabilities across strata. Kx1}
+#'   \item{\code{true_global_patterns}}{Matrix of true modal category values for 
+#'   each exposure item and latent class. pxK}
+#'   \item{\code{true_global_thetas}}{Array of true item level probabilities for 
+#'   each exposure item and latent class. pxKxR}
+#'   \item{\code{true_xi}}{Matrix of true probit regression model coefficients. Kxq}
+#'   \item{\code{true_Ci}}{Vector of true latent class assignments. nx1}
+#'   \item{\code{true_Phi_mat}}{Matrix of true outcome probabilities for each
+#'   class and stratum. KxS}
+#'   \item{\code{true_Phi}}{Vector of true outcome probabilities for all 
+#'   individuals. nx1}
+#' }
+#' @source Simulated data using `simulate_data.R`
+#' @keywords datasets
+#' @examples 
+#' data(sim_data)
+#' x_mat <- sim_data$X_data
+"sim_data"
---
+++ data-raw/data_nhanes.R
@@ -0,0 +1,131 @@
+
+#================= Code to prepare `data_nhanes` dataset =======================
+library(dplyr)
+# Download 'nhanes1518_adult_low_f_12jul2023.csv' file from 
+# https://github.com/smwu/SWOLCA. 
+
+# Set working directory to where the downloaded file is, and read in the dataset
+data_nhanes <- read.csv("./nhanes1518_adult_low_f_12jul2023.csv")
+
+# Clean variables
+data_nhanes <- data_nhanes %>% 
+  # Drop legumes (vegs) because duplicate of legumes (proteins), just computed
+  # as cup eq vs oz eq
+  select((-leg_veg)) %>%
+  # Drop those with refused or unknown education data
+  filter(!DMDEDUC2 %in% c(7, 0)) %>%
+  mutate(
+    # Get stratum IDs
+    stratum_id = SDMVSTRA,
+    # Create unique nested PSU IDs using stratum IDs
+    cluster_id = stratum_id * 10 + SDMVPSU,
+    # Get sampling weights
+    sample_wt = dietwt4yr,
+    # Categorize age
+    age_cat = factor(case_when(  
+      20 <= RIDAGEYR & RIDAGEYR <= 39 ~ 1,
+      40 <= RIDAGEYR & RIDAGEYR <= 59 ~ 2,
+      RIDAGEYR >= 60 ~ 3)),
+    # Re-categorize race and Hispanic origin ethnicity
+    # Original RIDRETH3 categories: 1=Mex_Amer, 2=Other_Hisp, 3=NH_White, 
+    # 4=NH_Black, 6=NH_Asian, 7=Other/Mixed
+    # New categories: 1=NH White, 2=NH Black, 3=NH Asian, 4=Hispanic, 5=Other/Mixed
+    racethnic = factor(case_when(
+      RIDRETH3 == 3 ~ 1,  # NH White
+      RIDRETH3 == 4 ~ 2,  # NH Black
+      RIDRETH3 == 6 ~ 3,  # NH Asian
+      RIDRETH3 %in% c(1, 2) ~ 4,  # Mexican-American/Other Hispanic
+      RIDRETH3 == 7 ~ 5,  # Other/Mixed
+          # RIDRETH3 == 1 ~ 4,  # Mexican-American
+          # RIDRETH3 == 2 ~ 5,  # Other Hispanic
+          # RIDRETH3 == 7 ~ 6,  # Other/Mixed
+      .default = NA)),
+    # Re-categorize education level
+    # Original DMDEDUC2 categories: 1= <9th, 2=9-11th, 3=HS/GED,
+    # 4=Some college/AA, 5=college grad or above, 7=refused, 9=don't know
+    # New DMDEDUC2 categories: 1=at least some college, 2=HS/GED, 3=less than HS
+    educ = factor(case_when(
+      DMDEDUC2 %in% c(4, 5) ~ 1,  # At least some college
+      DMDEDUC2 == 3 ~ 2,  # HS/GED
+      DMDEDUC2 %in% c(1, 2) ~ 3,  # Less than HS
+      .default = NA)),
+    # According to CDC Tobacco Glossary, define current smoker as having smoked 
+    # 100 cigarettes in lifetime and currently smokes cigarettes
+    # SMQ020: Smoked at least 100 cigarettes in life
+    # SMQ040 (nested): Now smoke cigarettes
+    smoker = factor(case_when(
+      SMQ040 %in% c(1, 2) ~ 1,  # current smoker
+      SMQ020 == 1 ~ 0,  # did not smoke 100 cigs in life
+      SMQ020 == 1 & SMQ040 == 3 ~ 0, # former smoker
+      .default = 0), levels = c(0, 1)),
+    # Physically active = 1 if >=150 mins of moderate or vigorous exercise per week
+    physactive = factor(
+      case_when(
+        Mins_Active >= 150 ~ 1,
+        Mins_Active < 150 ~ 0,
+        .default = NA), levels=c(0, 1), labels=c("Inactive", "Active")),
+    # BPQ020: Ever told you had high blood pressure
+    # BPQ030: Told had high blood pressure 2+ times
+    # BPQ050A: Now taking prescribed medicine for HBP
+    # Assign hypertension to those currently taking blood pressure medication
+    # or those told they had hypertension 2+ times
+    BP_flag = case_when(
+      SBP_avg > 130 ~ 1,  # Systolic BP above 130
+      DBP_avg > 80 ~ 1,  # Diagnostic BP above 80
+      BPQ030 == 1 ~ 1,  # Told high BP 2+ times
+      BPQ050A == 1 ~ 1,  # Taking BP medication
+      BPQ020 == 2 ~ 0,  # Never told had high blood pressure
+      .default = 0
+    ))
+
+# Keep only necessary variables
+data_nhanes <- data_nhanes %>%
+  select(SEQN, stratum_id, cluster_id, sample_wt, age_cat, racethnic, educ, 
+         smoker, physactive, BP_flag, citrus:drinks, cycle, nrecall, SDMVSTRA, 
+         SDMVPSU, RIDAGEYR, RIDRETH3, DMDEDUC2, SMQ020, SMQ040, 
+         Mins_Active, SBP_avg, DBP_avg, BPQ030, BPQ050A, BPQ020)
+
+usethis::use_data(data_nhanes, overwrite = TRUE)
+
+
+#================== Code to prepare NHANES results datasets ====================
+data("data_nhanes")
+x_mat <- as.matrix(dplyr::select(data_nhanes, citrus:drinks))
+y_all <- data_nhanes$BP_flag
+stratum_id <- data_nhanes$stratum_id
+cluster_id <- data_nhanes$cluster_id
+sampling_wt <- data_nhanes$sample_wt
+V_data <- dplyr::select(data_nhanes, age_cat, racethnic, smoker, physactive)
+glm_form <- "~ age_cat + racethnic + smoker + physactive"
+
+class_cutoff <- 0.05
+K_max <- 30
+alpha_adapt <- eta_adapt <- mu0_adapt <- Sig0_adapt <- NULL
+alpha_fixed <- eta_fixed <- mu0_fixed <- Sig0_fixed <- K_fixed <- fixed_seed <- NULL
+run_sampler <- "both"
+adapt_seed = 20230225
+n_runs = 20000
+burn = 10000
+thin = 5
+save_res = TRUE
+save_path = "~/data/"
+
+library(devtools)
+load_all()
+
+# runtime 2.878507 hours
+res_nhanes <- swolca(x_mat = x_mat, y_all = y_all, sampling_wt = sampling_wt,
+                     cluster_id = cluster_id, stratum_id = stratum_id, 
+                     V_data = V_data, run_sampler = "both", 
+                     glm_form = glm_form, adapt_seed = 20230225, 
+                     n_runs = 20000, burn = 10000, thin = 5, save_res = TRUE,
+                     save_path = "~/Downloads/run_nhanes")
+
+load("~/Downloads/run_nhanes_swolca_adapt.RData")
+run_nhanes_swolca_adapt <- res
+usethis::use_data(run_nhanes_swolca_adapt, overwrite = TRUE, compress = "xz")
+
+load("~/Downloads/run_nhanes_swolca_results.RData")
+run_nhanes_swolca_results <- res
+usethis::use_data(run_nhanes_swolca_results, overwrite = TRUE, compress = "xz")
+
